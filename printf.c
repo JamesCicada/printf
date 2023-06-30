17dd12e389317d410de3277070d21f5455d143f7
JamesCicada <mabdoox@gmail.com>
2023-06-30 08:42:32 -0700

    readme file

--- printf.c
+++ printf.c
@@ -14,6 +14,7 @@ int _printf(const char *format, ...)
 	int count = 0;
 	char c;
 	char *str;
+	int num;
 
 	va_start(args, format);
 
@@ -44,12 +45,19 @@ int _printf(const char *format, ...)
 					putchar('%');
 					count++;
 					break;
+				case 'd':
+					case 'i':
+					num = va_arg(args, int);
+					print_number(num);
+					count += get_num_length(num);
+					break;
 				default:
 					putchar('%');
 					putchar(*format);
 					count += 2;
 					break;
 			}
+			break;
 		}
 		else
 		{
@@ -63,5 +71,48 @@ int _printf(const char *format, ...)
 	va_end(args);
 
 	return count;
+}
+
+/**
+ * print_number - Prints an integer number
+ * @n: The number to print
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		putchar('-');
+		n = -n;
+	}
+
+	if (n / 10 != 0)
+		print_number(n / 10);
+
+	putchar((n % 10) + '0');
+}
+
+/**
+ * get_num_length - Computes the length of an integer number
+ * @n: The number
+ *
+ * Return: The length of the number
+ */
+int get_num_length(int n)
+{
+	int length = 0;
+
+	if (n < 0)
+	{
+		length++;
+		n = -n;
+	}
+
+	while (n != 0)
+	{
+		length++;
+		n /= 10;
+	}
+
+	return length;
 }
 
